72abacd268f194805f7e1ce98a2fc493d312b92c
Siddharth-Nama <siddharthnama2604@gmail.com>
2025-07-27 12:33:03 +0530

    Made post serializer and views.py and update urls.py file app app.demo

--- backend_repo/backend_repo/apps/demo/serializers.py
+++ backend_repo/backend_repo/apps/demo/serializers.py
@@ -1,1 +1,8 @@
 # TODO There's certainly more than one way to do this task, so take your pick.
+from rest_framework import serializers
+from .models import *
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = '__all__'
--- backend_repo/backend_repo/apps/demo/views.py
+++ backend_repo/backend_repo/apps/demo/views.py
@@ -1,1 +1,9 @@
 # TODO There's certainly more than one way to do this task, so take your pick.
+
+from rest_framework import generics
+from .models import Post
+from .serializers import PostSerializer
+
+class PostListView(generics.ListAPIView):
+    queryset = Post.objects.all().order_by('-timestamp')
+    serializer_class = PostSerializer
